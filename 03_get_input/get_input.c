89a5c19de1a74170932a2478b3a2c401e8f19b0f
Liam0241 <lleahy7288@floridapoly.edu>
2025-01-16 16:51:48 +0000

    square calculator

---
+++ 03_get_input/get_input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int square_side;
+    int perim = 0;
+    int area = 0;
+    printf("Side length: ");
+    scanf("%d", &square_side);
+    perim = square_side*4;
+    area = square_side*square_side;
+    printf("The perimiter is %d and the area is %d.\n", perim, area);
+    return 0;
+}
